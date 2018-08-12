cd51f5db57bc85819ae7337e13927b47e63b5dc0
phalbert <ahosea15@gmail.com>
2018-08-12 22:08:47 +0300

    added flash messages

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember me={}'.format(
-            form.username.data, form.remember_me.data)
+            form.username.data, form.remember_me.data
         ))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
