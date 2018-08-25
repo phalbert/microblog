10d2fa51c40f7d5d7f3d1e44049be61c0393e1d1
phalbert <ahosea15@gmail.com>
2018-08-25 13:38:15 +0300

    registration page

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db
+from app import login
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,7 +18,7 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
-        return check_password_hash(self.password_hash, passwords)
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,3 +28,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
