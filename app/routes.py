8fba03e62513ebc0b23d8e18e281621508846c84
phalbert <ahosea15@gmail.com>
2018-08-25 20:42:35 +0300

    user profile

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from werkzeug.urls import url_parse
 from flask_login import current_user, login_user
 from app.models import User
 from flask_login import login_required
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -60,3 +61,19 @@ def register():
         flash('You are now registered')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
